c2cd06809bfaf5e2cecf15b82fbb298b08518a73
Avotra dev <avotra@mkweblinks.com>
2024-10-10 09:01:10 +0200

    ios

--- ios/Sources/SpeakerPluginPlugin/SpeakerPluginPlugin.swift
+++ ios/Sources/SpeakerPluginPlugin/SpeakerPluginPlugin.swift
@@ -20,4 +20,35 @@ public class SpeakerPluginPlugin: CAPPlugin, CAPBridgedPlugin {
             "value": implementation.echo(value)
         ])
     }
+
+    @objc func setAudioMode(_ call: CAPPluginCall) {
+        guard let mode = call.getString("mode")?.lowercased() else {
+            call.reject("Mode est nul")
+            return
+        }
+
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(.playAndRecord, mode: .voiceChat, options: [])
+
+            switch mode {
+            case "speaker":
+                try audioSession.overrideOutputAudioPort(.speaker)
+                CAPLog.print("Speaker mode activated")
+            case "earpiece":
+                try audioSession.overrideOutputAudioPort(.none)
+                CAPLog.print("Earpiece mode activated")
+            default:
+                call.reject("Le mode doit être 'speaker' ou 'earpiece'")
+                return
+            }
+
+            try audioSession.setActive(true)
+            call.resolve([
+                "success": true
+            ])
+        } catch {
+            call.reject("Erreur lors de la configuration du mode audio: \(error.localizedDescription)")
+        }
+    }
 }
